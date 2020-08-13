ac198e927b0ed2c73fd3395296b2ee97bdef2193
Sam Grose <samugrose@gmail.com>
2020-08-13 16:16:05 -0700

    added img to portofliopanel, good looking link

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
-
+import './style.css'
+import Spon from './images/spont.png'
 export default function index() {
     return (
-        <div>
-            <section className="col-md-5 d-flex justify-content-center align-items-center">
-                <a href="https://spontaneouslynow.herokuapp.com/" target="_blank">
-                    <img src="images/spont.png" alt="Spontaneously Now!" className="img-fluid" />
-                        
-                    <figure className="row d-flex align-items-end justify-content-center">
+        <div className="container d-flex flex-column align-items-center">
+            <section className="row-md-12">
+                <h3 className="padded">Portfolio</h3>
+            </section>
+            <section className="row d-flex justify-content-around align-items-end">
+                <section className="col-md-5 d-flex justify-content-center align-items-center">
+                    <a href="https://spontaneouslynow.herokuapp.com/" target="_blank">
+                        <img src={Spon} alt="DEF!" className="img-fluid" />
+
+                        <figure className="row d-flex align-items-end justify-content-center">
                             <h6 className="hoverable">Spontaneously Now!</h6>
                         </figure>
-                </a>
+                    </a>
+                </section>
             </section>
-            
-        </div>
+        </div >
     )
 }
