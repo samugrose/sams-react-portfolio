804bd6450ee4cfaa62c75a13cc713b90f9c0290e
Sam Grose <samugrose@gmail.com>
2020-08-13 17:33:17 -0700

    Updates

---
+++ precache-manifest.2e12431215babf1cfe90f8250a1eac32.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf53ef1d2da0404d7c6914a5b00245da",
+    "url": "/samugrose/sams-react-portfolioindex.html"
+  },
+  {
+    "revision": "be0e18e835b8c521e8a2",
+    "url": "/samugrose/sams-react-portfoliostatic/css/main.96092ae9.chunk.css"
+  },
+  {
+    "revision": "7b0b70c1ab72e578074d",
+    "url": "/samugrose/sams-react-portfoliostatic/js/2.2a467ccf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/samugrose/sams-react-portfoliostatic/js/2.2a467ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0e18e835b8c521e8a2",
+    "url": "/samugrose/sams-react-portfoliostatic/js/main.a8ab9574.chunk.js"
+  },
+  {
+    "revision": "09aab49a3db97371d323",
+    "url": "/samugrose/sams-react-portfoliostatic/js/runtime-main.b3fe3a20.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/samugrose/sams-react-portfoliostatic/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ccbf3b4ccd298bc28682ca63d556d5bb",
+    "url": "/samugrose/sams-react-portfoliostatic/media/sams-headshot.ccbf3b4c.jpg"
+  },
+  {
+    "revision": "314fc437ddcdaec36ac4cf235610dfca",
+    "url": "/samugrose/sams-react-portfoliostatic/media/spont.314fc437.png"
+  }
+]);
