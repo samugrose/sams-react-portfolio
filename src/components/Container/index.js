9d26362ad87515df0d67fdd921227abe415d291c
Sam Grose <samugrose@gmail.com>
2020-08-13 17:06:41 -0700

    deleted container folder

--- src/components/Container/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function container() {
-    return (
-        <div className="container d-flex flex-column align-items-center">
-            
-        </div>
-    )
-}
